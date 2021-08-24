de7f44530ac8329cd4ef30c1597ca8cb6053e8b1
wspectra <dmgrishina@gmail.com>
2021-08-24 19:17:57 +0300

    00/00 done

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int k;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			k = 0;
+			while (argv[i][k] != '\0')
+			{
+				std::cout << (char)std::toupper(argv[i][k]);
+				k++;
+			}
+			std::cout << " ";
+			i++;
+		}
+		std::cout <<std::endl;
+	}
+	return (0);
+}
