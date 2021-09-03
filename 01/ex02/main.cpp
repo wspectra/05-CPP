f4f6adb6ac001b20d76d94d9dd2f88f64b9fa530
wspectra <dmgrishina@gmail.com>
2021-09-03 12:26:21 +0300

    0102

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -2,6 +2,17 @@
 
 int main()
 {
-	std::string s("HI THIS IS BRAIN");
-
+	std::string str("HI THIS IS BRAIN");
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	std::cout << "The address of the string                     "
+			  << &stringPTR << std::endl;
+	std::cout << "The address of the string using stringPTR     "
+	<< stringPTR << std::endl;
+	std::cout << "The address of the string using stringREF     "
+			  << &stringREF << std::endl;
+	std::cout << "display the string using stringPTR            \""
+	<< *stringPTR << "\"" << std::endl;
+	std::cout << "display the string using stringREF            \""
+			  << stringREF << "\""<<  std::endl;
 }
