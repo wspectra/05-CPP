dcbd7eb5f13b85bc6b500d174d081c38cc8fe424
wspectra <dmgrishina@gmail.com>
2021-09-10 13:30:07 +0300

    Clap start

---
+++ 03/ex01/ClapTrap.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+class ClapTrap {
+private:
+	std::string	name;
+	int			hitpoints;
+	int			energy;
+	int			damage;
+
+public:
+	ClapTrap(void);
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap &object);
+	~ClapTrap();
+	ClapTrap &operator=(const ClapTrap &object);
+	void attack(std::string const & target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
